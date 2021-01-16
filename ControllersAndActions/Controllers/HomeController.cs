0a9f884530f1d2de0b534cf8463432ce5d6c7e4d
flashpoint1986@mail.ru <flashpoint1986@mail.ru>
2021-01-17 01:40:58 +0300

    лолка и полка

--- ControllersAndActions/Controllers/HomeController.cs
+++ ControllersAndActions/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ControllersAndActions.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ControllersAndActions.Controllers
@@ -9,6 +11,9 @@ namespace ControllersAndActions.Controllers
     public class HomeController : Controller
     {
         public IActionResult Index() => View("SimpleForm");
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
         //public ViewResult ReceiveForm()
         //{
         //    var name = Request.Form["name"];
@@ -16,6 +21,30 @@ namespace ControllersAndActions.Controllers
         //    return View("Result", $"{name} живет в {city}");
         //}
 
-        public ViewResult ReceiveForm(string name, string city) => View("Result", $"Лошара {name} живет в {city}");
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        //public ViewResult ReceiveForm(string name, string city) => View("Result", $"Лошара {name} живет в {city}");
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        //public void ReceiveForm(string name, string city)
+        //{
+        //    Response.StatusCode = 200;
+        //    Response.ContentType = "text/html;charset=utf-8";
+        //    byte[] content = Encoding.UTF8.GetBytes($"<html><body><font color=red>{name} живет в {city}</font></body></html>");
+        //    Response.Body.WriteAsync(content, 0, content.Length);
+        //}
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        //public IActionResult ReceiveForm(string name, string city) => new CustomHtmlResult { Content = $"{name} влачит свое существование в {city}" };
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        //public IActionResult ReceiveForm(string name, string city) => Content($"{name} влачит свое жалкое существование в {city}");
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public ViewResult ReceiveForm(string name, string city) => View("Result", $"{name} влачит свое жалкое существование в {city}");
     }
 }
---
+++ ControllersAndActions/Infrastructure/CustomHtmlResult.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControllersAndActions.Infrastructure
+{
+    public class CustomHtmlResult : IActionResult
+    {
+        public string Content { get; set; }
+
+        public Task ExecuteResultAsync(ActionContext context)
+        {
+            context.HttpContext.Response.StatusCode = 200;
+            context.HttpContext.Response.ContentType = "text/html;charset=utf-8";
+            byte[] content = Encoding.UTF8.GetBytes(Content);
+            return context.HttpContext.Response.Body.WriteAsync(content, 0, content.Length);
+        }
+    }
+}
