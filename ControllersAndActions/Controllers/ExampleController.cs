9ba4efadf979af719d0724e6fd0e1e9da6d7e6a1
flashpoint1986@mail.ru <flashpoint1986@mail.ru>
2021-01-18 01:02:52 +0300

    Закончена глава 17

--- ControllersAndActions/Controllers/ExampleController.cs
+++ ControllersAndActions/Controllers/ExampleController.cs
@@ -15,5 +15,11 @@ namespace ControllersAndActions.Controllers
         //public RedirectToRouteResult Redirect() => RedirectToRoute(new { Controller = "Example", Action = "Result", id = "TestID" });
         //public RedirectToActionResult Redirect() => RedirectToAction(nameof(Result));
         public RedirectToActionResult Redirect() => RedirectToAction("Index", "Home");
+
+        public JsonResult Index2() => Json(new[] { "Ferrari", "Lamborghini", "Maserati" });
+        public ContentResult Index3() => Content("[\"Ferrari\",\"Lamborghini\",\"Maserati\"]");
+        public ObjectResult Index4() => Ok(new string[] { "Шла Саша по шоссе и сосала сушку" });
+        public VirtualFileResult Index5() => File("/dist/bootstrap/css/bootstrap.css", "text/css");
+        public StatusCodeResult Index6() => NotFound();
     }
 }
