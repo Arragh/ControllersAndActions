511b94c6795dd57e1f45d67f0c6b2ba67ae84d3f
flashpoint1986@mail.ru <flashpoint1986@mail.ru>
2021-01-17 22:05:33 +0300

    redirect

---
+++ ControllersAndActions/Controllers/ExampleController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControllersAndActions.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public ViewResult Index() => View(DateTime.Now);
+        public ViewResult Result() => View((object)"Hello redirect");
+
+        //public LocalRedirectResult Redirect() => LocalRedirect("google.com");
+        //public RedirectToRouteResult Redirect() => RedirectToRoute(new { Controller = "Example", Action = "Result", id = "TestID" });
+        //public RedirectToActionResult Redirect() => RedirectToAction(nameof(Result));
+        public RedirectToActionResult Redirect() => RedirectToAction("Index", "Home");
+    }
+}
